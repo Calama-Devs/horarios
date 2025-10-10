86a8ad73d85f6f1a2f5f5a77818409ad38a88d5e
Robert <robertterto04@gmail.com>
2025-10-10 14:53:56 -0400

    feat: Cria rotas necessarias para acesso publico e filtros

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,11 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
 $routes->get('/', 'Home::index');
+
+$routes->get('/horarios/publico', 'HorariosController::publico');
+
+$routes->post('/horarios/getTurmasByCurso', 'HorariosController::getTurmasByCurso');
+$routes->post('/horarios/filtrar', 'HorariosController::filtrar');
+
